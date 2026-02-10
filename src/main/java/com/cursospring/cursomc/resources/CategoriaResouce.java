2008bff90e1f184a894bd8d293bf4aa71763ed89
Jota Filho <j.filho-dti@hotmail.com>
2026-02-10 02:59:30 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/cursospring/cursomc/resources/CategoriaResouce.java
+++ src/main/java/com/cursospring/cursomc/resources/CategoriaResouce.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,28 +16,31 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.cursospring.cursomc.domain.Categoria;
 import com.cursospring.cursomc.services.CategoriaService;
 
-import ch.qos.logback.core.joran.spi.HttpUtil.RequestMethod;
-
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResouce {
 
-	@Autowired
-	private CategoriaService service;
+    @Autowired
+    private CategoriaService service;
 
-	@GetMapping(value = "/{id}")
-	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Categoria obj = service.buscar(id);
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Categoria> find(@PathVariable Integer id) {
+        Categoria obj = service.find(id);
+        return ResponseEntity.ok().body(obj);
+    }
 
-		return ResponseEntity.ok().body(obj);
-	}
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
 
-	@PostMapping
-	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
-		obj = service.insert(obj);
-
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-		return ResponseEntity.created(uri).build();
-
-	}
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
+        obj.setId(id); 
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
+    }
 }
